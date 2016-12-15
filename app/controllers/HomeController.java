0a9f1cbbfd30c3d4844e8feed3045d3f01a1d404
X00122527 <X001225270@gmail.com>
2016-12-15 13:27:56 +0000

    asd

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,20 @@
 package controllers;
 
+import controllers.*;
+import play.api.Environment;
 import play.mvc.*;
+import play.data.*;
+import play.db.ebean.Transactional;
+import models.users.*;
+import models.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
 
 import views.html.*;
+
+// Import models
+
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -16,61 +28,103 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
-    public Result index() {
-        return ok(index.render());
+
+    // Declare a private FormFactory instance
+    private FormFactory formFactory;
+
+    //  Inject an instance of FormFactory it into the controller via its constructor
+    @Inject
+    public HomeController(FormFactory f) {
+        this.formFactory = f;
+    }
+
+    private User getUserFromSession(){
+        return User.getUserById(session().get("email"));
+    }
+
+
+        public Result index() {
+        return ok(index.render(getUserFromSession()));
     }
 
 public Result whatson() {
-        return ok(whatson.render());
+        return ok(whatson.render(getUserFromSession()));
     }
 
 public Result booking() {
-        return ok(booking.render());
+        return ok(booking.render(getUserFromSession()));
     }
 
 public Result contact() {
-        return ok(contact.render());
+        return ok(contact.render(getUserFromSession()));
     }
 
-public Result signup() {
-        return ok(signup.render());
-    }
-
-public Result login() {
-        return ok(login.render());
-    }
+public Result signup() { return ok(signup.render(addUserForm, getUserFromSession()));}
 
 public Result inferno() {
-	return ok(inferno.render());
+	return ok(inferno.render(getUserFromSession()));
     }
 
 public Result arrival() {
-        return ok(arrival.render());
+        return ok(arrival.render(getUserFromSession()));
     }
 
 public Result jackreacher() {
-        return ok(jackreacher.render());
+        return ok(jackreacher.render(getUserFromSession()));
     }
 
 public Result trolls() {
-        return ok(trolls.render());
+        return ok(trolls.render(getUserFromSession()));
     }
 
 public Result gott() {
-        return ok(gott.render());
+        return ok(gott.render(getUserFromSession()));
     }
 
 public Result joneses() {
-        return ok(joneses.render());
+        return ok(joneses.render(getUserFromSession()));
     }
 
 public Result kubo() {
-        return ok(kubo.render());
+        return ok(kubo.render(getUserFromSession()));
     }
 
 public Result fantasticbeasts() {
-        return ok(fantasticbeasts.render());
+        return ok(fantasticbeasts.render(getUserFromSession()));
     }
 
+
+    public Result users(String email){
+        List<User> userList = new ArrayList<User>();
+
+
+            userList = User.findAll();
+
+        return ok(users.render(userList));
+
+    }
+
+    public Result addUser(){
+        Form<User> addUserForm = formFactory.form(User.class);
+
+        return ok(addUser.render(addUserForm, getUserFromSession()));
+    }
+    @Transactional
+    public Result addUserSubmit(){
+        Form<User> newUserForm = formFactory.form(User.class).bindFromRequest();
+        if(newUserForm.hasErrors()){
+            return badRequest(addUser.render(newUserForm, getUserFromSession()));
+        }
+        User u = newUserForm.get();
+
+        if(u.getEmail() == null){
+            u.save();
+        }
+        else if(u.getEmail() != null){
+            u.update();
+        }
+        flash("success", "User " + u.getEmail() + " has been registered.");
+        return redirect(controllers.routes.HomeController.users(null));
+    }
 
 }
--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -7,4 +7,4 @@ insert into user (email,name,password,role) values ( 'admin@products.com', 'Alic
 
 insert into user (email,name,password,role) values ( 'manager@products.com', 'Bob Manager', 'password', 'manager' );
 
-insert into user (email,name,password,role) values ( 'customer@products.com', 'Charlie Customer', 'password', 'customer' 
+insert into user (email,name,password,role) values ( 'customer@products.com', 'Charlie Customer', 'password', 'customer'); 
