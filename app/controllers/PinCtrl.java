3e26eab3afa6e383b469d41e210124d2618a2ed7
Jzyn <JasonPAP9@Gmail.com>
2017-03-30 00:32:38 +0100

    Almost there

---
+++ app/controllers/PinCtrl.java
@@ -0,0 +1,7 @@
+package controllers;
+
+/**
+ * Created by Jason on 29/03/2017.
+ */
+public class PinCtrl {
+}
---
+++ app/models/Pins.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by Jason on 29/03/2017.
+ */
+public class Pins {
+}
