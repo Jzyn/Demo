e808a35766c20ad146815bccbf08d2ba215d9a55
Enda Lee <enda.lee@ittdublin.ie>
2017-03-30 11:16:33 +0100

    mistakes were made

--- app/controllers/PinCtrl.java
+++
@@ -1,7 +0,0 @@
-package controllers;
-
-/**
- * Created by Jason on 29/03/2017.
- */
-public class PinCtrl {
-}
--- app/models/Pins.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-/**
- * Created by Jason on 29/03/2017.
- */
-public class Pins {
-}
